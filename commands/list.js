ffcef87f9513fad332c4d4fb7b769f532cee1917
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-07-10 14:30:17 +0100

    read last --argument lines

--- commands/list.js
+++ commands/list.js
@@ -1,9 +1,33 @@
 import chalk from "chalk";
+import fs from "fs";
+
+import { FILE_PATH } from "../lib/constants.js";
 import { isNumeric } from "../lib/utils.js";
 
 export function list(options) {
-    if (options.last && !isNumeric(options.last)) {
-        console.log(chalk.bgRed('Invalid option \'last\'! Must be a number'))
-        return;
+    try {
+        if (options.last && (!isNumeric(options.last) || parseInt(options.last) <= 0)) {
+            throw new Error('Invalid option \'last\'. It must be a positive number');
+        }
+
+        if (!fs.existsSync(FILE_PATH))
+            fs.writeFileSync(FILE_PATH, '', 'utf-8');
+    
+        const data = fs.readFileSync(FILE_PATH, 'utf-8');
+
+        if (!data) {
+            throw new Error('No data available');
+        }
+
+        const numOfRecordsToDisplay = options.last ? parseInt(options.last) : Number.MAX_SAFE_INTEGER;
+
+        const lines = data.trim().split('\n');
+        const linesToDisplay = (lines.length > numOfRecordsToDisplay) ? 
+                                    lines.slice(lines.length - numOfRecordsToDisplay, lines.length) : lines
+
+        
+
+    } catch (err) {
+        console.error(chalk.bgRed('An error occurred in the list command:'), chalk.red(err.message));
     }
 }
