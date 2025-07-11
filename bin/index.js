64c522baf3df5320a13cfe498160f497c6ee380d
Ricardo Oliveira <ricardoliveira5ro@gmail.com>
2025-07-11 09:25:02 +0100

    pin command

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,7 @@ import { program } from "commander";
 
 import { watch } from "../commands/watch.js";
 import { list } from "../commands/list.js";
+import { pin } from "../commands/pin.js";
 
 program
     .command('watch')
@@ -15,5 +16,11 @@ program
     .description('List saved clipboard entries with optional filters')
     .option('--last <number>', 'Show only the last <n> clipboard entries')
     .action((options) => list(options))
+
+program
+    .command('pin')
+    .description('Pin a clipboard entry by its ID')
+    .argument('<id>', 'Clipboard entry identifier')
+    .action((id) => pin(id))
 
 program.parse();
